f13e7af770f8139e78e0ef146b27621f132f219f
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-09-04 14:17:22 +0200

    Blend out cookie banner on accept/decline.

--- src/CookieBanner.tsx
+++ src/CookieBanner.tsx
@@ -31,12 +31,12 @@ export const CookieBanner = ()=>{
 
     return createPortal(
         <div className={`sticky bottom-0 bg-gray-800 p-5 text-white grid grid-cols-[1fr_auto]
-         ${cookiesAccepted !==undefined ?'opacity-0 pointer-events-none h-0 overflow-hidden p-0': 'opacity-100'}`}>
-            This page uses Google analytics to track page visits.
-            Are you okay with that?
-            <div className="grid gap-3 grid-cols-2">
+         ${cookiesAccepted !==undefined ?'opacity-0 pointer-events-none h-0 overflow-hidden hidden': 'opacity-100'}`}>
+            <span className="self-center">This page uses Google analytics to track page visits.
+                Are you okay with that?</span>
+            <div className="grid gap-3 grid-cols-2 self-center">
                 <button className="border-2 rounded" onClick={()=>setCookiesAccepted(false)}>Decline</button>
-                <button className="rounded bg-blue-600 p-2" onClick={()=>setCookiesAccepted(true)}>Accept cookies</button>
+                <button className="rounded bg-blue-600 p-2 self-center" onClick={()=>setCookiesAccepted(true)}>Accept cookies</button>
             </div>
         </div>
     , document.getElementById('banner')!)
