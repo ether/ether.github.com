0bc397fdbaffc4397400a97127fff336a51ee064
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-09-05 20:17:23 +0200

    Use browser router.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App, RootElement} from './App.tsx'
 import './index.css'
-import {createHashRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {createRoutesFromElements, Route} from "react-router";
 import {PluginViewer} from "./pages/PluginViewer.tsx";
 
 
-const router = createHashRouter(createRoutesFromElements(
+const router = createBrowserRouter(createRoutesFromElements(
     <Route element={<RootElement/>}>
         <Route index element={<App/>}/>
         <Route path="/plugins" element={<PluginViewer/>}/>
