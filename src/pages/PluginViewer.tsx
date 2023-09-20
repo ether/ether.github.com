491be04c4a405dbaabf01b5a577281fb6abb2735
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-09-20 21:14:36 +0200

    Added chips for keywords.

--- src/pages/PluginViewer.tsx
+++ src/pages/PluginViewer.tsx
@@ -1,16 +1,18 @@
 import {PluginResponse} from "../store/Plugin.ts";
 import {useUIStore} from "../store/store.ts";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import {PluginCom} from "../components/Plugin.tsx";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import {CheckIcon} from "../components/CheckIcon.tsx";
 
 export const PluginViewer = () => {
     const setPlugin = useUIStore(state => state.setPlugins)
     const plugins = useUIStore(state => state.plugins)
     const pluginSearchTerm = useUIStore(state => state.pluginSearchTerm)
     const setPluginSearchTerm = useUIStore(state => state.setPluginSearchTerm)
-
+    const [officalOnly, setOfficalOnly] = useState<boolean>(false)
 
     useEffect(() => {
         fetch('/api/plugins')
@@ -28,6 +30,17 @@ export const PluginViewer = () => {
             For more information about Etherpad visit https://etherpad.org.
         </span>
             <h2 className="text-3xl text-primary">Plugins ({plugins?.metadata.total_count})</h2>
+            <div className="flex gap-5">
+                <Checkbox.Root className="w-8 h-8 border-[1px] border-white p-2 grid place-items-center" checked={officalOnly}
+                               onCheckedChange={()=>setOfficalOnly(!officalOnly)} id="c1">
+                    <Checkbox.Indicator className="text-white">
+                            <CheckIcon/>
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
+                <label className="Label" htmlFor="c1">
+                    Accept terms and conditions.
+                </label>
+            </div>
             <div className="mt-5 mb-5 relative flex self-center w-full">
                 <input className="w-full  rounded border-[1px] pt-2 pb-2 pl-8 pr-1"
                        placeholder="Search for plugins to install" value={pluginSearchTerm}
