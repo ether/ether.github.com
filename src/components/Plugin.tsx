99e1e2f89da8e8c61fbd23795fa97802af608960
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-12-02 20:21:54 +0100

    Updated dependencies.

--- src/components/Plugin.tsx
+++ src/components/Plugin.tsx
@@ -21,49 +21,56 @@ const formatTime = (isoDate: string) => {
     return timeago.format(new Date(isoDate))
 }
 
-const renderMarkdown = (text: string) => {
-    const unsafeHtml = marked.parse(text)
-    const sanitizedHtml =  sanitizeHtml(unsafeHtml)
-    return {__html: sanitizedHtml}
-}
 
-export const PluginCom: FC<PluginProps> = ({plugins, index})=>{
-    const pluginResp = useUIStore(state=>state.plugins)
+
+export const PluginCom: FC<PluginProps> = ({plugins, index}) => {
+    const pluginResp = useUIStore(state => state.plugins)
     //const setPlugins = useUIStore(state=>state.setPlugins)
 
-    const loadNextPage = ()=> {
-    /*        fetch('/api/plugins')
-            .then(response => response.json())
-            .then((data: PluginResponse) => setPlugins(data))*/
+    const renderMarkdown =  (text: string) => {
+        const unsafeHtml =  marked.parse(text) as string
+        const sanitizedHtml = sanitizeHtml(unsafeHtml)
+        return {__html: sanitizedHtml}
     }
 
-    return  <div className="dark:text-white border-[1px] p-2 rounded">
+    const loadNextPage = () => {
+        /*        fetch('/api/plugins')
+                .then(response => response.json())
+                .then((data: PluginResponse) => setPlugins(data))*/
+    }
+
+    return <div className="dark:text-white border-[1px] p-2 rounded">
         <div className="flex">
-            {pluginResp && pluginResp?.plugins.length-2 === index && <Waypoint debug onEnter={()=>loadNextPage()}/>}
+            {pluginResp && pluginResp?.plugins.length - 2 === index && <Waypoint debug onEnter={() => loadNextPage()}/>}
             <div className="text-3xl text-primary flex gap-3">
                 <a target={"_blank"} href={'https://www.npmjs.org/package/' + plugins.name}>{plugins.name}</a>
                 <small className="align-text-bottom text-gray-400 mt-[3px]">{plugins.version}</small>
             </div>
             <div className="flex-grow"></div>
-            {plugins.time&&<div className="mr-5 mt-[0.3rem]">{formatTime(plugins.time)}</div>}
+            {plugins.time && <div className="mr-5 mt-[0.3rem]">{formatTime(plugins.time)}</div>}
             <div className="w-10 flex items-center mr-2">
-                <div className="w-10 border-[1px] border-white"  title={(plugins.popularity_score*100).toFixed(2)+"% popular among other plugins"}>
-                    <div style={{width: plugins.popularity_score*100+"%"}} className="bg-primary w-10 h-5 self-center"></div>
+                <div className="w-10 border-[1px] border-white"
+                     title={(plugins.popularity_score * 100).toFixed(2) + "% popular among other plugins"}>
+                    <div style={{width: plugins.popularity_score * 100 + "%"}}
+                         className="bg-primary w-10 h-5 self-center"></div>
                 </div>
             </div>
         </div>
         <div>{plugins.description}</div>
         <div className="flex gap-10">
-            {plugins.image&&<img alt={"Image of "+plugins.name} className="w-60 object-contain" src={plugins.image}/>}
-            {plugins.readme&&<div className="w-full line-clamp-5 readme-of-plugin" dangerouslySetInnerHTML={renderMarkdown(plugins.readme)}></div>}
+            {plugins.image &&
+                <img alt={"Image of " + plugins.name} className="w-60 object-contain" src={plugins.image}/>}
+            {plugins.readme && <div className="w-full line-clamp-5 readme-of-plugin"
+                                    dangerouslySetInnerHTML={renderMarkdown(plugins.readme)}></div>}
         </div>
         <div className="mt-5 flex">
             <PluginAuthorComp name={plugins.author} email={plugins.author_email}/>
             <span className="flex-grow"></span>
-            <span className="mr-5">License: {plugins.license? plugins.license: '--'}</span>
+            <span className="mr-5">License: {plugins.license ? plugins.license : '--'}</span>
             <span className="flex gap-3">
             {
-                plugins.keywords.trim().length>0 && plugins.keywords.split(",").map((k,i)=><Chip key={plugins.name+i} index={i}>{k}</Chip>)
+                plugins.keywords.trim().length > 0 && plugins.keywords.split(",").map((k, i) => <Chip
+                    key={plugins.name + i} index={i}>{k}</Chip>)
             }
             </span>
         </div>
