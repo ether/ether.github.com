1359dab73aabc1bcc61721a5f95ae6a4acdb99bf
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-08-11 20:10:15 +0200

    Fixed select version window.

--- src/components/SelectVersionModal.tsx
+++ src/components/SelectVersionModal.tsx
@@ -14,7 +14,47 @@ export const SelectVersionModal = ()=>{
         if(selectedVersionWindow && releases.length === 0){
             fetch('https://api.github.com/repos/ether/etherpad-lite/releases')
                 .then(response => response.json())
-                .then(data => {
+                .then((data: GHRelease[]) => {
+                    data.map((release:GHRelease)=>{
+                        if (release.assets.length === 0){
+                            release.assets = [{
+                                name: 'etherpad-lite-win.zip',
+                                browser_download_url: `https://github.com/ether/etherpad-lite/archive/refs/tags/${release.tag_name}.zip`,
+                                url: '',
+                                id: 0,
+                                node_id: '',
+                                label: '',
+                                uploader: {
+                                    "login": "github-actions[bot]",
+                                    "id": 41898282,
+                                    "node_id": "MDM6Qm90NDE4OTgyODI=",
+                                    "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+                                    "gravatar_id": "",
+                                    "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+                                    "html_url": "https://github.com/apps/github-actions",
+                                    "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+                                    "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+                                    "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+                                    "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+                                    "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+                                    "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+                                    "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+                                    "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+                                    "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+                                    "type": "Bot",
+                                    "site_admin": false
+                                },
+                                content_type: 'application/zip',
+                                state: 'uploaded',
+                                size: 0,
+                                download_count: 0,
+                                created_at: '',
+                                updated_at: ''
+                            }]
+                        }
+                    })
+
+
                     setReleases(data)
                 });
         }
